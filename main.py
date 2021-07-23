c7af00f61894c26cf8cf01601da182196796d440
jimyag <jimyag@126.com>
2021-07-23 22:19:15 +0800

    更新requirements

--- main.py
+++ main.py
@@ -1,2 +1,9 @@
+from scripts.schedule import Schedule
+import sys
+from PyQt5.QtWidgets import QDialog, QFileDialog, QApplication
+
 if __name__ == '__main__':
-    print(1)
+    q_app = QApplication(sys.argv)
+    schedule = Schedule()
+    schedule.show()
+    sys.exit(q_app.exec_())
